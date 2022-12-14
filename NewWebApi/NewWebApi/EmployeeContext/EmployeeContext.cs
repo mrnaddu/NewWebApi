9351cc49e5f81502ccd05a0974224db43a5656c1
mrnaddu <mohammadnadm5@gmail.com>
2022-12-14 14:41:55 +0530

    InnitialPush

---
+++ NewWebApi/NewWebApi/EmployeeContext/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewWebApi.Model;
+
+namespace NewWebApi.EmployeeContext
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options) : base(options)
+        {
+        }
+        DbSet<Employee> Employees { get; set; }
+    }
+
+    
+}
---
+++ NewWebApi/NewWebApi/Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace NewWebApi.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name  { get; set; }
+
+        public long Salary { get; set; }
+
+        public long Number { get; set; }
+    }
+}
